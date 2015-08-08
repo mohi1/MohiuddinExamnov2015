3cd7c9b97c220b70936e7f282d7aa01ad42211f9
mrahman <mohammad.rahman@hbo.com>
2015-08-08 13:47:53 -0400

    +Map

---
+++ src/datastructure/UseMap.java
@@ -0,0 +1,15 @@
+package datastructure;
+
+public class UseMap {
+
+	public static void main(String[] args) {
+		/*
+		 * Demonstrate how to use Map that includes storing and retrieving elements.
+		 * Use nested Map. Map<String, List<string>> list = new HAshMap<String, List<String>>();
+		 * Use For Each loop and while loop with Iterator to retrieve data.
+		 * 
+		 */
+
+	}
+
+}
